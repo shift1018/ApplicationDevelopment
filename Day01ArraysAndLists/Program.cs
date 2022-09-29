ea86de80a147ff20de9e60d5429e80df772a6dfb
Raymond <shift10180701@gmail.com>
2022-09-29 13:03:54 -0400

    day1

---
+++ Day01ArraysAndLists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01ArraysAndLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // possibly a jagged array, only the first dimension is allocated
+            int[][] twoDimInt = new int[4][];
+            // rectangular array
+            int[,] twoDimIntRectangular = new int[4, 3];
+            int[,,] threeDimIntRect = new int[4, 3, 2]; // 3D: 4 x 3 x 2 size
+
+            int[,] data2dInts = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 18 } };
+            int x = 0;
+            float avg;
+            float sum = 0;
+            for ( int i = 0; i<data2dInts.GetLength(0); i++)
+            {
+                for (int j = 0; j < data2dInts.GetLength(1); j++)
+                {
+                    
+                    sum += data2dInts[i, j];
+                    x++;
+                   
+                }
+            }
+            avg = sum / x;
+            Console.WriteLine(sum); 
+            Console.WriteLine(x); 
+            Console.WriteLine("{ avg: 000}");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        
+    } 
+   
+}
---
+++ Day01TextFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Day01TextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine();
+            string[] username3 = {username,username, username};
+
+            System.IO.File.WriteAllText("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines1.txt", username);
+            System.IO.File.WriteAllLines("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines2.txt", username3);
+            string shift = System.IO.File.ReadAllText("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines2.txt");
+            Console.WriteLine(shift);
+            Console.ReadKey();
+        }
+    }
+}
