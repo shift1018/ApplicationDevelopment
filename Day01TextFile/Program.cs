5dad6c36a9fb7f0ed5f8c7a5bd6c4fe0e6c332bc
Raymond <shift10180701@gmail.com>
2022-10-03 10:30:14 -0400

    update after lessons

--- Day01TextFile/Program.cs
+++ Day01TextFile/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Net.WebRequestMethods;
+using File = System.IO.File;
 
 namespace Day01TextFile
 {
@@ -11,15 +13,65 @@ namespace Day01TextFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string username = Console.ReadLine();
-            string[] username3 = {username,username, username};
+            try { 
+            //Console.WriteLine("Enter username:");
+            //string username = Console.ReadLine();
+            //string[] username3 = { username, username, username };
 
-            System.IO.File.WriteAllText("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines1.txt", username);
-            System.IO.File.WriteAllLines("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines2.txt", username3);
-            string shift = System.IO.File.ReadAllText("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines2.txt");
-            Console.WriteLine(shift);
-            Console.ReadKey();
+            //System.IO.File.WriteAllText("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines1.txt", username);
+            //System.IO.File.WriteAllLines("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines2.txt", username3);
+            //string shift = System.IO.File.ReadAllText("C:\\Users\\shift\\Documents\\GitHub\\AppDev\\ApplicationDevelopment\\Day01TextFile\\WriteLines2.txt");
+            //Console.WriteLine(shift);
+            //Console.ReadKey();
+
+            const string filePath = @"C:\Users\shift\Documents\GitHub\AppDev\ApplicationDevelopment\Day01TextFile\WriteLines1.txt";
+
+            Console.WriteLine("what is your name");
+            string name = Console.ReadLine();
+
+            try
+            {
+
+            
+                {
+                    string[] namesArray = { name, name, name };
+                    File.WriteAllLines(filePath, namesArray);
+                }
+                {
+                    string filecontents = $"{name}\n{name}\n{name}\n";
+                    File.WriteAllText(filePath, filecontents);
+                }
+            }
+            catch(SystemException ex) {
+                Console.WriteLine("Error write to file: " + ex.Message);
+            }
+            try
+            {
+                    {
+                        string[] linesArray = File.ReadAllLines(filePath);
+                        foreach(String line in linesArray)
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                    {
+                        string allContent = File.ReadAllText(filePath);
+                        Console.WriteLine(allContent);
+                    }
+
+            }
+            catch(SystemException ex)
+                {
+                    Console.WriteLine("Error writing to file: " + ex.Message);
+                }
+
+
+
+
+                } finally{
+                    Console.WriteLine("press any key to finish");
+                    Console.ReadKey();
+                }
         }
     }
 }
