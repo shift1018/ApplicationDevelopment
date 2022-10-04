b4c30500d58a5c5e213ccf04e8d07e6f8fadf0ac
Raymond <shift10180701@gmail.com>
2022-10-04 13:43:34 -0400

    multitempconv

---
+++ AllInputs/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AllInputs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //string name = TbxName.Text;
+            ////fixme:validate
+            //if (name == "")
+            //{
+            //    MessageBox.Show("Name must not be empty!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //    return;
+            //}
+
+            //MessageBox.Show($"Hello {name}，nice to meet you!", "Greeting", MessageBoxButton.OK, MessageBoxImage.Information);
+
+           // < Label Content = "What is your name?" HorizontalAlignment = "Left" Margin = "10,10,0,0" VerticalAlignment = "Top"  Width = "107" FontSize = "11" />
+       // < TextBox x: Name = "TbxName" HorizontalAlignment = "Left" Margin = "152,13,0,0" TextWrapping = "Wrap" Text = "" VerticalAlignment = "Top" Width = "109" RenderTransformOrigin = "0.133,-2.283" />
+        //< Button Content = "Say Hello (label)" HorizontalAlignment = "Left" Margin = "19,47,0,0" VerticalAlignment = "Top" Click = "btnSayHelloLabel_Click" />
+        //< Button Content = "Say Hello (msg box)" HorizontalAlignment = "Left" Margin = "152,47,0,0" VerticalAlignment = "Top" RenderTransformOrigin = "0.478,0.603" Click = "btnSayHelloPopuo_Click" />
+       // < Label x: Name = "LblGreeting" Content = "..." HorizontalAlignment = "Left" Margin = "19,87,0,0" VerticalAlignment = "Top" Width = "251" />
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
---
+++ multitempconv/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace multitempconv
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+  
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        //private void RbninputCelsius_Checked(object sender, RoutedEventArgs e)
+        //{
+
+
+
+
+        //}
+
+        //private void RbninputFahrenheit_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    double input = Double.Parse(inputbox.Text);
+
+        //    double output;
+        //    if (RbnoutputCelsius.IsChecked == true)
+        //    {
+        //        output = (input - 32) * 5 / 9;
+        //        outputbox.Text = output.ToString();
+        //    }
+        //    else if (RbnoutputFahrenheit.IsChecked == true)
+        //    {
+        //        output = input;
+        //        outputbox.Text = output.ToString();
+        //    }
+        //    else if (RbnoutputKelvin.IsChecked == true)
+        //    {
+        //        output =;
+        //        outputbox.Text = output.ToString();
+        //    }
+
+        //}
+
+        //private void RbninputKelvin_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    double input = Double.Parse(inputbox.Text);
+
+        //    double output;
+        //    if (RbnoutputCelsius.IsChecked == true)
+        //    {
+        //        output = input - 273.15;
+        //        outputbox.Text = output.ToString();
+        //    }
+        //    else if (RbnoutputFahrenheit.IsChecked == true)
+        //    {
+        //        output = (input - 273.15) * 9 / 5 + 32;
+        //        outputbox.Text = output.ToString();
+        //    }
+        //    else if (RbnoutputKelvin.IsChecked == true)
+        //    {
+        //        output = input;
+        //        outputbox.Text = output.ToString();
+        //    }
+        //}
+
+        private void RbnoutputCelsius_Checked(object sender, RoutedEventArgs e)
+        {
+            double input = double.Parse(inputbox.Text);
+            Console.WriteLine(input);
+
+
+            double output;
+            if (RbninputCelsius.IsChecked == true)
+            {
+                output = input;
+                outputbox.Text = output.ToString();
+            }
+            else if (RbninputFahrenheit.IsChecked == true)
+            {
+                output = (input -32)*5/9;
+                outputbox.Text = output.ToString();
+            }
+            else if (RbninputKelvin.IsChecked == true)
+            {
+                output = input - 273.15;
+                outputbox.Text = output.ToString();
+            }
+        }
+
+        private void RbnoutputFahrenheit_Checked(object sender, RoutedEventArgs e)
+        {
+            double input = double.Parse(inputbox.Text);
+            Console.WriteLine(input);
+
+
+            double output;
+            if (RbninputCelsius.IsChecked == true)
+            {
+                output = input*9/5 +32;
+                outputbox.Text = output.ToString();
+            }
+            else if (RbninputFahrenheit.IsChecked == true)
+            {
+                output = input;
+                outputbox.Text = output.ToString();
+            }
+            else if (RbninputKelvin.IsChecked == true)
+            {
+                output = (input - 273.15) *9/5+32;
+                outputbox.Text = output.ToString();
+            }
+        }
+
+        private void RbnoutputKelvin_Checked(object sender, RoutedEventArgs e)
+        {
+            double input = double.Parse(inputbox.Text);
+            Console.WriteLine(input);
+
+
+            double output;
+            if (RbninputCelsius.IsChecked == true)
+            {
+                output = input+273.15;
+                outputbox.Text = output.ToString();
+            }
+            else if (RbninputFahrenheit.IsChecked == true)
+            {
+                output = ((input - 32) * 5 / 9) + 273.15;
+                outputbox.Text = output.ToString();
+            }
+            else if (RbninputKelvin.IsChecked == true)
+            {
+                output = input;
+                outputbox.Text = output.ToString();
+            }
+        }
+    }
+}
